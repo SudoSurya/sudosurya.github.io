335f2f4c30c6b4014df2839ba99d3b1336dd7330
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2024-02-19 23:24:39 +0530

    intial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Technologies used:
+  - Astro
+  - Tailwindcss
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,astro}"],
+  theme: {
+    fontFamily: {
+      sans: ['"Montserrat"'],
+    },
+    colors: {
+      black: "#202232",
+      navy: "#2B2D42",
+      white: "#EDF2F4",
+      crimson: "#EF233C",
+      red: "#F0374E",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
